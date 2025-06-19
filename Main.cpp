4b90440b420f5df5a4bb418a6a145c07585bd176
Tsion2116 <tsionmulugeta1111@gmail.com>
2025-06-19 11:33:56 +0300

    add the "init" and "add" functions to the Main.cpp file

--- Main.cpp
+++ Main.cpp
@@ -6,7 +6,7 @@
 #include<vector>
 #include<ctime>
 #include<direct.h>
-
+using namespace std;
 //---------------------Data Structures----------------------
 
 
@@ -18,6 +18,44 @@
 
 
 //---------------------Main Functions---------------------
+ void init(){
+    if (dirExists(".minigit")){
+        cout<<"MiniGit repository already exists.\n";
+        return;
+       }
+       _mkdir(".minigit");
+       _mkdir(".minigit/objects");
+       _mkdir(".minigit/refs");
+       // initial commit
+       Commit initialCommit;
+       initialComit.hash =computeHash("init");
+       initialCommit.message = "Initial commit";
+       initialCommit.timestamp = time(nullptr);     
+         // Save initial commit 
+         ofstream commitFile(".minigit/objects/" + initialCommit.hash);
+            commitFile << initialCommit.message << "\n" << initialCommit.timestamp << "\n";
+         commitFile.close();
+         // Create HEAD
+            ofstream headFile(".minigit/HEAD");
+            headFile << "ref: refs/heads/master";
+            headFile.close();
+            cout << "Initialized empty MiniGit repository\n";
+ }
+void add(const string& filename) {
+    if (!fileExists(filename)) {
+        cerr << "Error: File '" << filename << "' does not exist.\n";
+        return;
+    }
+
+    string content = readFile(filename);
+    string hash = computeHash(content); 
+    // Save blob in .minigit/objects
+    ofstream blobFile(".minigit/objects/" + hash);
+    blobFile << content;        
+    blobFile.close();   
+    stagedFiles.insert(filename);
+    cout << "Added '" << filename << "' to staging area.\n";
+}   
 
 //---------------------CLI interface---------------------
 
